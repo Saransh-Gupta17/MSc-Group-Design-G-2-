65137f2b4dca62f0623e48af741295a08d6868be
Saransh-Gupta17 <yoursaransh@gmail.com>
2026-03-14 01:59:37 +0000

    Basically, wing.m now has no hard coded values and takes all its variable values from the ADP itself.

--- cadem0016-master/+B777/+geom/wing.m
+++ cadem0016-master/+B777/+geom/wing.m
@@ -1,37 +1,45 @@
 function [GeomObj, massObj] = wing(obj)
+% wing.m
+% Class I wing geometry + mass model (Raymer-based)
+% Supports baseline and folding-wing configurations
+%
+% Baseline: pure Raymer Class I
+% Folding : first-order span + hinge mass penalties
 
+%% ============================================================
+%                 PLANFORM GEOMETRY
+% =============================================================
 
-%% ---------------- Planform geometry ----------------
-
+% Quarter-chord sweep estimate (transonic transport)
 SweepQtrChord = real(acosd(0.75 .* obj.Mstar ./ obj.TLAR.M_c));
 
+% Empirical taper ratio (Raymer-style)
 tr = -0.0083 * SweepQtrChord + 0.4597;
 
 b = obj.Span;
-
-S = obj.WingArea;
+S = (obj.MTOM * 9.81) / obj.WingLoading;
 
 R_f = obj.CabinRadius;
+L2 = obj.KinkPos - R_f;       % fuselage to kink
+L3 = b/2 - obj.KinkPos;       % kink to tip
 
-L2 = obj.KinkPos - R_f;
-L3 = b/2 - obj.KinkPos;
-
+% Initial chord guess
 c_r_star = (S/b) / (1 + tr);
-
 c0 = (1 - (1-tr)*obj.KinkPos/(b/2)) * c_r_star;
 
+% Solve for chord that gives correct area
 c = fminsearch(@(x)(get_areas(x,L2,L3,R_f,tr,SweepQtrChord) - S).^2, c0);
+[~, c_t, c_r, A1, A2, A3] = get_areas(c,L2,L3,R_f,tr,SweepQtrChord);
 
-[~, c_t, c_r] = get_areas(c,L2,L3,R_f,tr,SweepQtrChord);
-% Compute Mean Aerodynamic Chord (MAC)
-lambda = c_t / c_r;
-obj.c_ac = (2/3) * c_r * (1 + lambda + lambda^2) / (1 + lambda);
-
-sweepLE = atand((tand(SweepQtrChord)*L3 + c/4 - c_t/4)/L3);
+%% ============================================================
+%                 PLANFORM POINTS
+% =============================================================
 
 ys = [-b/2 -obj.KinkPos -R_f 0 R_f obj.KinkPos b/2]';
-
 cs = [c_t c c_r c_r c_r c c_t]';
+
+sweepLE   = atand((tand(SweepQtrChord)*L3 + c/4 - c_t/4)/L3);
+sweepHalf = atand((tand(SweepQtrChord)*L3 - c/4 + c_t/4)/L3);
 
 x_le = [ ...
     tand(sweepLE)*(L2+L3)
@@ -43,126 +51,71 @@ x_le = [ ...
     tand(sweepLE)*(L2+L3) ];
 
 x_le = x_le - 0.25*c_r;
-
 x_te = x_le + cs;
 
 Xs = [x_le ys; flipud(x_te) flipud(ys)];
 
-Xs(:,1) = Xs(:,1) + obj.WingPos;
-
 GeomObj = cast.GeomObj(Name="Wing", Xs=Xs);
 
-% CLASS II.5 STRUCTURAL WING MASS MODEL
+%% ============================================================
+%                 CLASS I WING MASS (RAYMER)
+% =============================================================
 
+b_w = obj.Span * SI.ft;               % span [ft]
+S_w = obj.WingArea * SI.ft^2;         % area [ft^2]
+t_w = 0.15 * c_r * SI.ft;             % max thickness at root [ft]
 
-span = obj.Span;
-S = obj.WingArea;
-MTOM = obj.MTOM;
+% IMPORTANT: use half-chord sweep (matches reference code)
+cosLambda = cosd(sweepHalf);
 
-g = 9.81;
+% Design gross weight [lb]
+Wdg_lb = obj.MTOM * obj.Mf_TOC * SI.lb;
 
-%% Load cases
+% Ultimate load factor
+n_z = 2.5 * 1.5;
 
-n_cases = [2.5 2.0 2.3]; % manoeuvre, landing, gust
+% Raymer wing weight [lb]
+w_wing = 0.00125 * Wdg_lb * (b_w/cosLambda)^0.75 * ...
+    (1 + sqrt(6.3*cosLambda/b_w)) * n_z^0.55 * ...
+    (b_w*S_w/(t_w*Wdg_lb*cosLambda))^0.3;
 
-W = MTOM*g;
-L_cases = n_cases * W;
+% Baseline wing mass [kg]
+m_wing_base = w_wing / SI.lb;
 
-N = 200;
-y = linspace(0,span/2,N);
+%% ============================================================
+%                 FOLDING-WING CORRECTION (CLASS I)
+% =============================================================
 
-M_roots = zeros(size(L_cases));
+m_wing = m_wing_base;
 
-for i = 1:length(L_cases)
+if isfield(obj,'WingConfig') && obj.WingConfig == "folding"
 
-    q0 = (4*L_cases(i))/(pi*span);
-    q  = q0*sqrt(1-(2*y/span).^2);
-
-    V = cumtrapz(flip(y),flip(q));
-    V = flip(V);
-
-    M = cumtrapz(flip(y),flip(V));
-    M = flip(M);
-
-    M_roots(i) = M(1);
-
-end
-
-M_design = max(M_roots);
-
-%% Wing box geometry
-
-lambda = 0.3;
-
-c_root = 2*S/(span*(1+lambda));
-
-box_height = 0.12*c_root;
-flange_width = 0.4*c_root;
-
-%% Material properties
-
-sigma_allow = 250e6; % Pa
-rho_al = 2800; % kg/m^3
-
-%% Section modulus requirement
-
-Z_req = M_design / sigma_allow;
-
-A_skin_root = (2*Z_req)/box_height;
-
-t_root = A_skin_root/(2*flange_width);
-
-%% Spanwise thickness distribution
-
-t_span = t_root * sqrt(1-(2*y/span).^2);
-
-%% Skin volume
-
-skin_volume = 2 * trapz(y, flange_width .* t_span);
-
-%% Spar volume (tapered)
-
-t_spar_root = 0.02;
-t_spar_tip  = 0.005;
-
-t_spar_span = linspace(t_spar_root,t_spar_tip,N);
-
-spar_volume = 2 * trapz(y, box_height .* t_spar_span);
-
-%% Rib estimation
-
-rib_spacing = 0.6;
-n_ribs = span / rib_spacing;
-
-rib_area = flange_width * box_height;
-rib_volume = n_ribs * rib_area * 0.0015;
-
-%% Structural volume
-
-structure_volume = skin_volume + spar_volume + rib_volume;
-
-structure_mass = rho_al * structure_volume;
-
-%% Control surface mass
-
-control_surface_mass = 0.05 * structure_mass;
-
-wing_mass = structure_mass + control_surface_mass;
-
-%% Return wing mass
-
-massObj = cast.MassObj(Name="Wing", m=wing_mass, X=[obj.WingPos; 0]);
+    % k_span  : global bending-induced mass increase
+    % k_hinge : local hinge + reinforcement mass
+    m_wing = m_wing_base * (1 + obj.k_span + obj.k_hinge);
 
 end
 
+%% ============================================================
+%                 MASS OBJECT
+% =============================================================
 
-%% -------- Wing area helper --------
-function [S,c_t,c_r] = get_areas(c,L2,L3,R_f,tr,SweepQtrChord)
+massObj = cast.MassObj( ...
+    Name = "Wing", ...
+    m    = m_wing, ...
+    X    = [obj.WingPos; 0] );
+
+end
+
+%% ============================================================
+%                 HELPER FUNCTION
+% =============================================================
+
+function [S,c_t,c_r,A1,A2,A3] = get_areas(c,L2,L3,R_f,tr,SweepQtrChord)
 
 c_t = tr * c;
 
 sweepLE = atand((tand(SweepQtrChord)*L3 + c/4 - c_t/4)/L3);
-
 c_r = c + tand(sweepLE)*L2;
 
 A1 = c_r * R_f;
@@ -170,6 +123,5 @@ A2 = (c_r + c)/2 * L2;
 A3 = (c + c_t)/2 * L3;
 
 S = 2*(A1 + A2 + A3);
-
 
 end
